b25f461216fb4cfb54000c2be3f5a108f438939b
ericktheredd5875 <ericktheredd5875@gmail.com>
2025-12-03 17:08:15 -0800

    [feat] cli entry points

--- dicerealms/__init__.py
+++ dicerealms/__init__.py
@@ -1,1 +1,6 @@
 # SPDX-License-Identifier: MIT
+
+from dicerealms.protocol.messages import ActionMessage, ChatMessage, ConnectMessage
+from dicerealms.server.server import GameServer
+
+__all__ = ["GameServer", "ConnectMessage", "ActionMessage", "ChatMessage"]
---
+++ dicerealms/__main__.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: MIT
+import asyncio
+
+from dicerealms.cli import app
+from dicerealms.console_frontend import ConsoleFrontend
+
+
+async def main():
+    frontend = ConsoleFrontend()
+    await frontend.run()
+
+
+def app():  # noqa: F811
+    """Entry point for the CLI application."""
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
